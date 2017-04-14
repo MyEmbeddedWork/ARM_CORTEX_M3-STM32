6f7923c55abea8ba12932c0be44eb55624f63c6b
AnilHomePC <aniljoseph007@gmail.com>
2017-04-14 19:07:33 +0530

    New project files are created

--- 3. Codes/1. Basic/src/main.cpp
+++ 3. Codes/1. Basic/src/main.cpp
@@ -32,7 +32,7 @@
 // The external clock frequency is specified as a preprocessor definition
 // passed to the compiler via a command line option (see the 'C/C++ General' ->
 // 'Paths and Symbols' -> the 'Symbols' tab, if you want to change it).
-// The value selected during project creation was HSE_VALUE=8000000.
+// The value selected during project creation was HSE_VALUE=7200000.
 //
 // Note: The default clock settings take the user defined HSE_VALUE and try
 // to reach the maximum possible system clock. For the default 8 MHz input
